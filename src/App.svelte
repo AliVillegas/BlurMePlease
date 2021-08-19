0fd2eba48d0db546bd0c15aa6da69c64bf8ba433
Ali Villegas <84879600+SofthealthAli@users.noreply.github.com>
2021-08-19 15:21:08 -0500

    functional blurring
    Missing about and how it works pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
+  import { Jumper } from "svelte-loading-spinners";
+
   let chosenImage = null;
+  let showSpinner = false;
+  let blurStrength = 50;
+  let fileTooBig = false;
   function refreshPage() {
     window.location.reload();
   }
@@ -8,11 +13,23 @@
     chosenImage = e.target.files[0];
   }
   function doSomeMagic() {
+    fileTooBig = false;
+    showSpinner = true;
     console.log("MAGIC");
     let image = new Image();
+    if (chosenImage === null) {
+      showSpinner = false;
+      return;
+    } else {
+      if (chosenImage.size > 2097152) {
+        fileTooBig = true;
+        this.value = "";
+        showSpinner = false;
+        return;
+      }
+    }
     image.src = URL.createObjectURL(chosenImage);
     image.onload = function () {
-      console.log(image);
       if (image) {
         manipulateImage(image);
       }
@@ -21,21 +38,22 @@
 
   function manipulateImage(i) {
     let mat = cv.imread(i);
-    let l = 5;
-    let u = 6;
-
-    cv.cvtColor(mat, mat, cv.COLOR_BGR2GRAY);
-    let gaussianBlurred = new cv.Mat();
-    let ksize = new cv.Size(5, 5);
-
     document.getElementById("downloadButton").style.display = "block";
+    let gaussianBlurred = new cv.Mat();
+    let strength = parseFloat(blurStrength * 2);
+    if (!(strength % 2)) {
+      strength = strength + 1;
+    }
+    console.log("sizes", strength);
 
-    cv.GaussianBlur(mat, gaussianBlurred, ksize, 0, 0, cv.BORDER_DEFAULT);
+    let ksize = new cv.Size(strength, strength);
+    cv.GaussianBlur(mat, gaussianBlurred, ksize, cv.BORDER_DEFAULT);
     cv.imshow("outputImage", gaussianBlurred);
-    let secondGrayPass = new cv.Mat();
-    cv.cvtColor(gaussianBlurred, secondGrayPass, cv.COLOR_BGR2GRAY);
-    cv.imshow("outputImage", secondGrayPass);
+    window.scrollTo(0, document.body.scrollHeight);
+    showSpinner = false;
+    //delete matrices
     mat.delete();
+    gaussianBlurred.delete();
   }
 
   function download() {
@@ -53,6 +71,7 @@
   />
   <script async src="js/opencv.js"></script>
 </svelte:head>
+
 <nav class="navbar navbar-expand-lg navbar-light borderBottomPurple mb-3">
   <img
     on:click={refreshPage}
@@ -75,16 +94,32 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mx-auto my-2 p-3 ">
-      <li class="nav-item">
+      <li class="nav-item mx-auto p-2">
         <a class="textPurple font" href="https://www.patreon.com/alivillegas"
-          >Donate</a
+          >How it works</a
         >
+      </li>
+      <li class="nav-item mx-auto p-2">
+        <a class="textPurple font" href="">About</a>
+      </li>
+      <li class="nav-item mx-auto p-2">
+        <a class="textPurple font" href="">Donate</a>
       </li>
     </ul>
   </div>
 </nav>
 <div class="mx-auto d-block text-center my-5 font">
-  <h3>Simply choose a PNG file, click the button and watch some magic</h3>
+  <h3 class="text-right">
+    1. Simply choose a PNG or JPG file. (maximum 2MB) <p />
+    2. Select blurriness strength (100 is extra blurry)
+    <p />
+    3. Click Blur! and hopefully watch some magic
+  </h3>
+  {#if fileTooBig}
+    <h1 class="text-right my-5" style="color:rebeccaPurple">
+      That file is too big (╯°□°）╯︵ ┻━┻
+    </h1>
+  {/if}
 </div>
 <div class="mx-auto d-block text-center my-3">
   <input
@@ -98,10 +133,38 @@
     required
   />
   <p />
+  <div class="slidecontainer font">
+    <input
+      style="width:50vw;background-color:rebeccapurple"
+      type="range"
+      min="1"
+      max="100"
+      bind:value={blurStrength}
+      on:change={console.log(blurStrength)}
+      class="slider"
+      id="myRange"
+    />
+    <p />
+    Strength:
+    <h4 class="text-right">
+      {blurStrength}
+    </h4>
+  </div>
+  <p />
   <button class="begoneBtn borderPurple my-3" on:click={doSomeMagic}>
-    <span style="margin-bottom: 10;"> Begone! </span>
+    <span style="margin-bottom: 10;"> Blur! </span>
   </button>
 </div>
+{#if showSpinner}
+  <div class="row">
+    <div class="col-5" />
+    <div class="col-4 mx-auto text-center">
+      <Jumper size="90" color="#7a0bea" unit="px" duration="2s" />
+    </div>
+    <div class="col-3" />
+  </div>
+{/if}
+
 <div class="mx-auto d-block text-center my-5">
   <button
     class="begoneBtn borderPurple my-5 mx-auto "
@@ -114,7 +177,7 @@
 </div>
 <div class="mx-auto d-block text-center my-3">
   <canvas id="outputImage" width="500" />
-  <img id="input_img" width="500" height="500" alt="" style="display:none" />
+  <img id="input_img" alt="" style="display:none" />
 </div>
 
 <style>
